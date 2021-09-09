18d811ffebcda0e99daa0906d165c5210f7e2573
robertojssouza <robertojssouza@yahoo.com.br>
2021-09-09 18:21:44 -0300

    Dividindo a função contador em duas funções mais simples

--- python/pomodoro.py
+++ python/pomodoro.py
@@ -1,30 +1,33 @@
 from time import sleep
 import os
 
-
-def contador():
+def leiaSeg():
     os.system('cls' if os.name == 'nt' else 'clear')
+    
     while True:
         try:
             t = int(input('Digite quantos minutos você vai trabalhar entre as pausas: ')) * 60
-            while t:
-                min = t // 60
-                sec = t % 60
-                os.system('cls' if os.name == 'nt' else 'clear')
-                print(f'{min:02d}:{sec:02d}')
-                sleep(1)
-                t -= 1
-            print('Está na hora de esticar o esqueleto, beber água ou café')
-            sleep(10)
-            break
-
+            return t
+        
         except (ValueError, TypeError):
             print('Digite um valor válido')
             continue
-
-        except (KeyboardInterrupt):
-            print('O usuário interrompeu o programa.')
-            break
+           
+     
+def contador():
+    try:
+        t = leiaSeg()
+        while t:
+            min = t // 60
+            sec = t % 60
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(f'{min:02d}:{sec:02d}')
+            sleep(0.2)
+            t -= 1
+        print('Está na hora de esticar o esqueleto, beber água ou café')
+        sleep(10)
+    except (KeyboardInterrupt):
+        print('O usuário interrompeu o programa.')
 
 
 
